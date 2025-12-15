e9f4b713afe2fa465fd7308d5ae4364c5b2d7616
Bastien <bastien-stachs@hotmail.fr>
2025-12-15 16:50:42 +0100

    fallback pour les routes non trouvées

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article.details');
+Route::fallback(function () { return view('errors.not-found'); });
